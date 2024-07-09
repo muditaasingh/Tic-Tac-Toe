e11e4c3c47608ca279c52b8baf6e1457b165d93c
MUDITA SINGH <136784096+muditaasingh@users.noreply.github.com>
2024-07-09 13:02:08 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import Board from './Board';
+import './App.css';
+
+function App() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const current = history[stepNumber];
+  const winner = calculateWinner(current);
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+    if (winner || squares[i]) return;
+    squares[i] = xIsNext ? 'X' : 'O';
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setXIsNext(!xIsNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext(step % 2 === 0);
+  };
+
+  const resetGame = () => {
+    setHistory([Array(9).fill(null)]);
+    setStepNumber(0);
+    setXIsNext(true);
+  };
+
+  return (
+    <div className="game">
+      <h1>TIC-TAC-TOE</h1>
+      <Board squares={current} onClick={handleClick} />
+      <div className="game-info">
+        <div>{winner ? 'Winner: ' + winner : 'Next Player: ' + (xIsNext ? 'X' : 'O')}</div>
+        <button onClick={resetGame}>RESET</button>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+export default App;
